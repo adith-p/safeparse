d050675724989e220c215db777161ad427395167
adith-p <adithprakash008@gmail.com>
2025-09-10 21:23:05 +0530

    created custom key-pair fn, added 2 new config folder

--- utils/encryption/EncryptionManager.py
+++ utils/encryption/EncryptionManager.py
@@ -5,6 +5,8 @@ import gnupg
 class EncryptionManager:
     def __init__(self, username: str, email: str) -> None:
         self.config_path: Path = Path("~/.safeparse/gpg").expanduser()
+        self.export_folder: Path = Path("~/.safeparse/gpg/export_keys").expanduser()
+        self.import_folder: Path = Path("~/.safeparse/gpg/import_keys").expanduser()
         self.username: str = username
         self.email: str = email
         self.key_type: str = "RSA"
@@ -13,6 +15,8 @@ class EncryptionManager:
 
     def _create_config_folder(self):
         self.config_path.mkdir(parents=True, exist_ok=True)
+        self.export_folder.mkdir(parents=True, exist_ok=True)
+        self.import_folder.mkdir(parents=True, exist_ok=True)
 
     def init_encryption(self):
         self._create_config_folder()
@@ -26,8 +30,35 @@ class EncryptionManager:
             passphrase=password,
             key_type=self.key_type,
             key_length=self.key_length,
+            name_real=self.username,
         )
         self.gpg.gen_key(key_input)
 
     def get_all_keys(self):
         return self.gpg.list_keys()
+
+    def custom_create_keys(
+        self,
+        real_name: str,
+        passphrase: str,
+        name_comment: str,
+        name_email: str,
+        key_type: str = "RSA",
+        key_length: str = "4096",
+    ):
+        key_input = self.gpg.gen_key_input(
+            name_email=name_email,
+            key_type=key_type,
+            key_length=key_length,
+            name_real=real_name,
+            name_comment=name_comment,
+            passphrase=passphrase,
+        )
+        self.gpg.gen_key(key_input)
+
+    def revoke_key(self):
+        enc = self.gpg
+
+    def delete_key(self, fingerprint):
+        self.delete_key(fingerprint)
+        print("[bold green] Key deleted [/bold green]")
