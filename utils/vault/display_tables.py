3ae0655ce6d3189962ba8ffe3885a1cce43218d1
adith-p <adithprakash008@gmail.com>
2025-09-14 16:53:39 +0530

    added display_table_contact()

--- utils/vault/display_tables.py
+++ utils/vault/display_tables.py
@@ -19,3 +19,20 @@ def display_tables(values):
         table.add_row(*val, style="bold green")
 
     print(table)
+
+
+def display_tables_contact(values):
+    table = Table()
+    cols = [
+        "contact id",
+        "contact name",
+        "contact email",
+        "key fingerprint",
+    ]
+
+    for col in cols:
+        table.add_column(col)
+    for val in values:
+        table.add_row(*val, style="bold green")
+
+    print(table)
