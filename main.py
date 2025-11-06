82e4d3ff6888ae9e341a19b8ea069bfa7161f7d2
adith-p <adithprakash008@gmail.com>
2025-11-06 11:05:21 +0530

    feat: inactivity exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from prompt_toolkit import PromptSession
 from rich import print
-
+import time
 
 from safeparse.core.dispatcher import dispatch
 
@@ -8,9 +8,12 @@ from safeparse.core.dispatcher import dispatch
 from safeparse.core.startup_fn import init_db
 from safeparse.core.users.user_auth import create_user, is_authenticated
 from safeparse.logging.logger import logger
+from safeparse.core.users.user_auth import set_auth
 
 
 # Interactive loop
+
+
 def main():
     # NOTE: .log can be tampered with as of now so, once the encryption module is set up i will encrypt and hash the .log file
     logger.info("Application starting...")
@@ -29,16 +32,32 @@ def main():
     print("Type [bold] auth user [/bold] to continue")
     logger.info("Application started")
     session = PromptSession()
+
+    INACTIVITY_TIME = 10
+    last_time_out = time.time()
     while True:
         try:
+            if is_authenticated():
 
+                current_time = time.time()
+                if (current_time - last_time_out) > float(INACTIVITY_TIME):
+                    set_auth(False)
+                    logger.warning("User session timed out due to inactivity.")
+                    print(
+                        "\n[bold yellow]Session timed out due to inactivity. Please log in again.[/bold yellow]"
+                    )
+                    break
+
+#            last_time_out = time.time()
             prompt_text = "(auth-cli) " if is_authenticated() else "(un-auth) "
             text = session.prompt(prompt_text)
-
+            last_time_out = time.time()
             if dispatch(text) is False:
                 break
 
         except KeyboardInterrupt:
+
+            last_time_out = time.time()
             continue
         except EOFError:
             break
